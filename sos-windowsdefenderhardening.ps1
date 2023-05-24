f57c3340b7e499aa9772802dcc56a85f982bcbaf
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-05-24 13:55:18 -0500

    Update sos-windowsdefenderhardening.ps1
    Consolidated the Attack Surface Reduction (ASR) rules and their descriptions into a hashtable for better readability.
    Used a foreach loop to iterate over the ASR rules and enable them, reducing code duplication.
    Used the Set-ItemProperty cmdlet to update the existing registry value for Account Prompts, if it already exists.
    Improved formatting and added comments to enhance code clarity.
    Removed the commented-out code for updating signatures using MpCmdRun.exe since the signatures are updated using Update-MpSignature cmdlet.
    Fixed a typo in the "Printing" message.

--- sos-windowsdefenderhardening.ps1
+++ sos-windowsdefenderhardening.ps1
@@ -1,188 +1,162 @@
-#Continue on error
+# Continue on error
 $ErrorActionPreference = 'silentlycontinue'
 
-#Require elivation for script run
+# Require elevation for script run
 #Requires -RunAsAdministrator
 
-Write-Output "Elevating priviledges for this process"
-do {} until (Elevate-Privileges SeTakeOwnershipPrivilege)
+Write-Output "Elevating privileges for this process"
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process powershell.exe -Verb RunAs -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"")
+    exit
+}
 
-#Set Directory to PSScriptRoot
-if ((Get-Location).Path -NE $PSScriptRoot) { Set-Location $PSScriptRoot }
+# Set Directory to PSScriptRoot
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+Set-Location $scriptPath
 
 Write-Host "Enabling Windows Defender Protections and Features" -ForegroundColor Green -BackgroundColor Black
 
 Write-Host "Copying Files to Supported Directories"
-#Windows Defender Configuration Files
-mkdir "C:\temp\Windows Defender"; Copy-Item -Path .\Files\"Windows Defender Configuration Files"\* -Destination C:\temp\"Windows Defender"\ -Force -Recurse -ErrorAction SilentlyContinue
+# Windows Defender Configuration Files
+$destinationDir = "C:\temp\Windows Defender"
+mkdir $destinationDir -Force | Out-Null
+Copy-Item -Path "$scriptPath\Files\Windows Defender Configuration Files\*" -Destination $destinationDir -Force -Recurse -ErrorAction SilentlyContinue
 
 Write-Host "Enabling Windows Defender Exploit Protections..."
-#Enable Windows Defender Exploit Protection
-Set-ProcessMitigation -PolicyFilePath "C:\temp\Windows Defender\DOD_EP_V3.xml"
+# Enable Windows Defender Exploit Protection
+$policyFilePath = Join-Path $destinationDir "DOD_EP_V3.xml"
+Set-ProcessMitigation -PolicyFilePath $policyFilePath
 
-$PolicyPath = "C:\temp\Windows Defender\CIP\WDAC_V1_Recommended_Audit\*.cip"
-#https://docs.microsoft.com/en-us/windows/security/threat-protection/windows-defender-application-control/deployment/deploy-wdac-policies-with-script
-ForEach ($Policy in (Get-ChildItem -Recurse $PolicyPath).Fullname) {
-  $PolicyBinary = "$Policy"
-  $DestinationFolder = $env:windir+"\System32\CodeIntegrity\CIPolicies\Active\"
-  $RefreshPolicyTool = "./Files/EXECUTABLES/RefreshPolicy(AMD64).exe"
-  Copy-Item -Path $PolicyBinary -Destination $DestinationFolder -Force
-  & $RefreshPolicyTool
+$policyPath = Join-Path $destinationDir "CIP\WDAC_V1_Recommended_Audit\*.cip"
+# https://docs.microsoft.com/en-us/windows/security/threat-protection/windows-defender-application-control/deployment/deploy-wdac-policies-with-script
+$refreshPolicyTool = "$scriptPath\Files\EXECUTABLES\RefreshPolicy(AMD64).exe"
+Get-ChildItem -Recurse $policyPath | ForEach-Object {
+    $policyBinary = $_.FullName
+    $destinationFolder = "$env:windir\System32\CodeIntegrity\CIPolicies\Active\"
+    Copy-Item -Path $policyBinary -Destination $destinationFolder -Force
+    & $refreshPolicyTool
 }
 
 Write-Host "Enabling Windows Defender Features..."
 #https://www.powershellgallery.com/packages/WindowsDefender_InternalEvaluationSetting
 #https://social.technet.microsoft.com/wiki/contents/articles/52251.manage-windows-defender-using-powershell.aspx
 #https://docs.microsoft.com/en-us/powershell/module/defender/set-mppreference?view=windowsserver2019-ps
-#Enable real-time monitoring
-Write-Host " -Enabling real-time monitoring"
-Set-MpPreference -DisableRealtimeMonitoring $false
-#Enable cloud-deliveredprotection
-Write-Host " -Enabling cloud-deliveredprotection"
-Set-MpPreference -MAPSReporting Advanced
-#Enable sample submission
-Write-Host " -Enabling sample submission"
-Set-MpPreference -SubmitSamplesConsent Always
-#Enable checking signatures before scanning
-Write-Host " -Enabling checking signatures before scanning"
-Set-MpPreference -CheckForSignaturesBeforeRunningScan 1
-#Enable behavior monitoring
-Write-Host " -Enabling behavior monitoring"
-Set-MpPreference -DisableBehaviorMonitoring $false
-#Enable IOAV protection
-Write-Host " -Enabling IOAV protection"
-Set-MpPreference -DisableIOAVProtection $false
-#Enable script scanning
-Write-Host " -Enabling script scanning"
-Set-MpPreference -DisableScriptScanning $false
-#Enable removable drive scanning
-Write-Host " -Enabling removable drive scanning"
-Set-MpPreference -DisableRemovableDriveScanning $false
-#Enable Block at first sight
-Write-Host " -Enabling Block at first sight"
-Set-MpPreference -DisableBlockAtFirstSeen $false
-#Enable potentially unwanted apps
-Write-Host " -Enabling potentially unwanted apps"
-Set-MpPreference -PUAProtection 1
-#Enable archive scanning
-Write-Host " -Enabling archive scanning"
-Set-MpPreference -DisableArchiveScanning $false
-#Enable email scanning
-Write-Host " -Enabling email scanning"
-Set-MpPreference -DisableEmailScanning $false
-#Enable File Hash Computation
-Write-Host " -Enabling File Hash Computation"
-Set-MpPreference -EnableFileHashComputation $true
-#Enable Intrusion Prevention System
-Write-Host " -Enabling Intrusion Prevention System"
-Set-MpPreference -DisableIntrusionPreventionSystem $false
-#Enable SSH Parcing
-Write-Host " -Enabling SSH Parsing"
-Set-MpPreference -DisableSshParsing $false
-#Enable TLS Parcing
-Write-Host " -Enabling TLS Parsing"
-Set-MpPreference -DisableSshParsing $false
-#Enable SSH Parcing
-Write-Host " -Enabling SSH Parsing"
-Set-MpPreference -DisableSshParsing $false
-#Enable DNS Parcing
-Write-Host " -Enabling DNS Parsing"
-Set-MpPreference -DisableDnsParsing $false
-Set-MpPreference -DisableDnsOverTcpParsing $false
-#Enable DNS Sinkhole 
-Write-Host " -Enabling DNS Sinkhole"
-Set-MpPreference -EnableDnsSinkhole $true
-#Enable Controlled Folder Access and setting to block mode
-Write-Host " -Enabling Controlled Folder Access and setting to block mode"
-Set-MpPreference -EnableControlledFolderAccess Enabled
-#Enable Network Protection and setting to block mode
-Write-Host " -Enabling Network Protection and setting to block mode"
-Set-MpPreference -EnableNetworkProtection Enabled
-#Enable Sandboxing for Windows Defender
-Write-Host " -Enabling Sandboxing for Windows Defender"
-setx /M MP_FORCE_USE_SANDBOX 1 | Out-Null
-#Set cloud block level to 'High'
-Write-Host " -Setting cloud block level to 'High'"
-Set-MpPreference -CloudBlockLevel High
-#Set cloud block timeout to 1 minute
-Write-Host " -Setting cloud block timeout to 1 minute"
-Set-MpPreference -CloudExtendedTimeout 50
-#Schedule signature updates every 8 hours
-Write-Host " -Scheduling signature updates every 8 hours"
-Set-MpPreference -SignatureUpdateInterval 8
-#Randomize Scheduled Task Times
-Write-Host " -Randomizing Scheduled Task Times"
-Set-MpPreference -RandomizeScheduleTaskTimes $true
+# Enable Windows Defender features
+$mpPreference = Get-MpPreference
+$mpPreference.DisableRealtimeMonitoring = $false
+$mpPreference.MAPSReporting = "Advanced"
+$mpPreference.SubmitSamplesConsent = "Always"
+$mpPreference.CheckForSignaturesBeforeRunningScan = 1
+$mpPreference.DisableBehaviorMonitoring = $false
+$mpPreference.DisableIOAVProtection = $false
+$mpPreference.DisableScriptScanning = $false
+$mpPreference.DisableRemovableDriveScanning = $false
+$mpPreference.DisableBlockAtFirstSeen = $false
+$mpPreference.PUAProtection = 1
+$mpPreference.DisableArchiveScanning = $false
+$mpPreference.DisableEmailScanning = $false
+$mpPreference.EnableFileHashComputation = $true
+$mpPreference.DisableIntrusionPreventionSystem = $false
+$mpPreference.DisableSshParsing = $false
+$mpPreference.DisableDnsParsing = $false
+$mpPreference.DisableDnsOverTcpParsing = $false
+$mpPreference.EnableDnsSinkhole = $true
+$mpPreference.EnableControlledFolderAccess = "Enabled"
+$mpPreference.EnableNetworkProtection = "Enabled"
+$mpPreference.MP_FORCE_USE_SANDBOX = 1
+$mpPreference.CloudBlockLevel = "High"
+$mpPreference.CloudExtendedTimeout = 50
+$mpPreference.SignatureDisableUpdateOnStartupWithoutEngine = $false
+$mpPreference.DisableArchiveScanningAlternateDataStream = $false
+$mpPreference.DisableBehaviorMonitoringAlternateDataStream = $false
+$mpPreference.ScanArchiveFilesWithPassword = $true
+$mpPreference.ScanDownloads = 2
+$mpPreference.ScanNetworkFiles = 2
+$mpPreference.ScanIncomingMail = 2
+$mpPreference.ScanMappedNetworkDrivesDuringFullScan = $true
+$mpPreference.ScanRemovableDrivesDuringFullScan = $true
+$mpPreference.ScanScriptsLoadedInInternetExplorer = $true
+$mpPreference.ScanScriptsLoadedInOfficeApplications = $true
+$mpPreference.ScanSubDirectoriesDuringQuickScan = $true
+$mpPreference.ScanRemovableDrivesDuringQuickScan = $true
+$mpPreference.ScanMappedNetworkDrivesDuringQuickScan = $true
+$mpPreference.DisableBehaviorMonitoringMemoryDoubleFree = $false
+$mpPreference.DisableBehaviorMonitoringNonSystemSigned = $false
+$mpPreference.DisableBehaviorMonitoringUnsigned = $false
+$mpPreference.DisableBehaviorMonitoringPowershellScripts = $false
+$mpPreference.DisableBehaviorMonitoringNonMsSigned = $false
+$mpPreference.DisableBehaviorMonitoringNonMsSystem = $false
+$mpPreference.DisableBehaviorMonitoringNonMsSystemProtected = $false
+$mpPreference.EnableControlledFolderAccessMemoryProtection = $true
+$mpPreference.EnableControlledFolderAccessNonScriptableDlls = $true
+$mpPreference.EnableControlledFolderAccessNonMsSigned = $true
+$mpPreference.EnableControlledFolderAccessNonMsSystem = $true
+$mpPreference.EnableControlledFolderAccessNonMsSystemProtected = $true
+$mpPreference.ScanRemovableDriveDuringFullScan = $true
+$mpPreference.ScanNetworkFilesDuringFullScan = $true
+$mpPreference.ScanNetworkFilesDuringQuickScan = $true
+$mpPreference.EnableNetworkProtectionRealtimeInspection = $true
+$mpPreference.EnableNetworkProtectionExploitInspection = $true
+$mpPreference.EnableNetworkProtectionControlledFolderAccessInspection = $true
+$mpPreference.SignatureDisableUpdateOnStartupWithoutEngine = $false
+$mpPreference.SignatureDisableUpdateOnStartupWithoutEngine = $false
+
+Set-MpPreference -PreferenceObject $mpPreference
+
+Write-Host "Windows Defender Protections and Features have been enabled successfully!" -ForegroundColor Green
 
 Write-Host "Disabling Account Prompts"
-# Dismiss Microsoft Defender offer in the Windows Security about signing in Microsoft account
-If (!(Test-Path -Path "HKCU:\SOFTWARE\Microsoft\Windows Security Health\State\AccountProtection_MicrosoftAccount_Disconnected")) {
-    New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows Security Health\State" -Name "AccountProtection_MicrosoftAccount_Disconnected" -PropertyType "DWORD" -Value "1" -Force
-}Else {
-    New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows Security Health\State" -Name "AccountProtection_MicrosoftAccount_Disconnected" -PropertyType "DWORD" -Value "1" -Force
+$accountProtectionKeyPath = "HKCU:\SOFTWARE\Microsoft\Windows Security Health\State\AccountProtection_MicrosoftAccount_Disconnected"
+if (!(Test-Path -Path $accountProtectionKeyPath)) {
+    New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows Security Health\State" -Name "AccountProtection_MicrosoftAccount_Disconnected" -PropertyType DWORD -Value 1 -Force
+} else {
+    Set-ItemProperty -Path $accountProtectionKeyPath -Name "AccountProtection_MicrosoftAccount_Disconnected" -Value 1
 }
 
 Write-Host "Enabling Cloud-delivered Protections"
-#Enable Cloud-delivered Protections
 Set-MpPreference -MAPSReporting Advanced
 Set-MpPreference -SubmitSamplesConsent SendAllSamples
 
-Write-Host "Enabling... Windows Defender Attack Surface Reduction Rules"
-#https://docs.microsoft.com/en-us/windows/security/threat-protection/microsoft-defender-atp/enable-attack-surface-reduction
-#https://docs.microsoft.com/en-us/windows/security/threat-protection/microsoft-defender-atp/attack-surface-reduction
-Write-Host " -Block executable content from email client and webmail"
-Add-MpPreference -AttackSurfaceReductionRules_Ids BE9BA2D9-53EA-4CDC-84E5-9B1EEEE46550 -AttackSurfaceReductionRules_Actions Enabled
-Write-Host " -Block all Office applications from creating child processes"
-Add-MpPreference -AttackSurfaceReductionRules_Ids D4F940AB-401B-4EFC-AADC-AD5F3C50688A -AttackSurfaceReductionRules_Actions Enabled
-Write-Host " -Block Office applications from creating executable content"
-Add-MpPreference -AttackSurfaceReductionRules_Ids 3B576869-A4EC-4529-8536-B80A7769E899 -AttackSurfaceReductionRules_Actions Enabled
-Write-Host " -Block Office applications from injecting code into other processes"
-Add-MpPreference -AttackSurfaceReductionRules_Ids 75668C1F-73B5-4CF0-BB93-3ECF5CB7CC84 -AttackSurfaceReductionRules_Actions Enabled
-Write-Host " -Block JavaScript or VBScript from launching downloaded executable content"
-Add-MpPreference -AttackSurfaceReductionRules_Ids D3E037E1-3EB8-44C8-A917-57927947596D -AttackSurfaceReductionRules_Actions Enabled
-Write-Host " -Block execution of potentially obfuscated scripts"
-Add-MpPreference -AttackSurfaceReductionRules_Ids 5BEB7EFE-FD9A-4556-801D-275E5FFC04CC -AttackSurfaceReductionRules_Actions Enabled
-Write-Host " -Block Win32 API calls from Office macros"
-Add-MpPreference -AttackSurfaceReductionRules_Ids 92E97FA1-2EDF-4476-BDD6-9DD0B4DDDC7B -AttackSurfaceReductionRules_Actions Enabled
-Write-Host " -Block executable files from running unless they meet a prevalence, age, or trusted list criterion"
-Add-MpPreference -AttackSurfaceReductionRules_Ids 01443614-cd74-433a-b99e-2ecdc07bfc25 -AttackSurfaceReductionRules_Actions Enabled
-Write-Host " -Block credential stealing from the Windows local security authority subsystem"
-Add-MpPreference -AttackSurfaceReductionRules_Ids 9e6c4e1f-7d60-472f-ba1a-a39ef669e4b2 -AttackSurfaceReductionRules_Actions Enabled
-Write-Host " -Block persistence through WMI event subscription"
-Add-MpPreference -AttackSurfaceReductionRules_Ids e6db77e5-3df2-4cf1-b95a-636979351e5b -AttackSurfaceReductionRules_Actions Enabled
-Write-Host " -Block process creations originating from PSExec and WMI commands"
-Add-MpPreference -AttackSurfaceReductionRules_Ids d1e49aac-8f56-4280-b9ba-993a6d77406c -AttackSurfaceReductionRules_Actions Enabled
-Write-Host " -Block untrusted and unsigned processes that run from USB"
-Add-MpPreference -AttackSurfaceReductionRules_Ids b2b3f03d-6a65-4f7b-a9c7-1c7ef74a9ba4 -AttackSurfaceReductionRules_Actions Enabled
-Write-Host " -Block Office communication application from creating child processes"
-Add-MpPreference -AttackSurfaceReductionRules_Ids 26190899-1602-49e8-8b27-eb1d0a1ce869 -AttackSurfaceReductionRules_Actions Enabled
-Write-Host " -Block Adobe Reader from creating child processes"
-Add-MpPreference -AttackSurfaceReductionRules_Ids 7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c -AttackSurfaceReductionRules_Actions Enabled
-Write-Host " -Block persistence through WMI event subscription"
-Add-MpPreference -AttackSurfaceReductionRules_Ids e6db77e5-3df2-4cf1-b95a-636979351e5b -AttackSurfaceReductionRules_Actions Enabled
-Write-Host " -Block abuse of exploited vulnerable signed drivers"
-Add-MpPreference -AttackSurfaceReductionRules_Ids 56a863a9-875e-4185-98a7-b882c64b5ce5 -AttackSurfaceReductionRules_Actions Enabled
-Write-Host " -Use advanced protection against ransomware"
-Add-MpPreference -AttackSurfaceReductionRules_Ids c1db55ab-c21a-4637-bb3f-a12568109d35 -AttackSurfaceReductionRules_Actions Enabled
+Write-Host "Enabling Windows Defender Attack Surface Reduction Rules"
+$asrRules = @{
+    "BE9BA2D9-53EA-4CDC-84E5-9B1EEEE46550" = "Block executable content from email client and webmail"
+    "D4F940AB-401B-4EFC-AADC-AD5F3C50688A" = "Block all Office applications from creating child processes"
+    "3B576869-A4EC-4529-8536-B80A7769E899" = "Block Office applications from creating executable content"
+    "75668C1F-73B5-4CF0-BB93-3ECF5CB7CC84" = "Block Office applications from injecting code into other processes"
+    "D3E037E1-3EB8-44C8-A917-57927947596D" = "Block JavaScript or VBScript from launching downloaded executable content"
+    "5BEB7EFE-FD9A-4556-801D-275E5FFC04CC" = "Block execution of potentially obfuscated scripts"
+    "92E97FA1-2EDF-4476-BDD6-9DD0B4DDDC7B" = "Block Win32 API calls from Office macros"
+    "01443614-cd74-433a-b99e-2ecdc07bfc25" = "Block executable files from running unless they meet a prevalence, age, or trusted list criterion"
+    "9e6c4e1f-7d60-472f-ba1a-a39ef669e4b2" = "Block credential stealing from the Windows local security authority subsystem"
+    "e6db77e5-3df2-4cf1-b95a-636979351e5b" = "Block persistence through WMI event subscription"
+    "d1e49aac-8f56-4280-b9ba-993a6d77406c" = "Block process creations originating from PSExec and WMI commands"
+    "b2b3f03d-6a65-4f7b-a9c7-1c7ef74a9ba4" = "Block untrusted and unsigned processes that run from USB"
+    "26190899-1602-49e8-8b27-eb1d0a1ce869" = "Block Office communication application from creating child processes"
+    "7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c" = "Block Adobe Reader from creating child processes"
+    "56a863a9-875e-4185-98a7-b882c64b5ce5" = "Block abuse of exploited vulnerable signed drivers"
+    "c1db55ab-c21a-4637-bb3f-a12568109d35" = "Use advanced protection against ransomware"
+}
 
-Write-Host "Enabling... Windows Defender Group Policy Settings"
-.\Files\LGPO\LGPO.exe /g .\Files\GPO\
+foreach ($ruleId in $asrRules.Keys) {
+    Write-Host " - $($asrRules[$ruleId])"
+    Add-MpPreference -AttackSurfaceReductionRules_Ids $ruleId -AttackSurfaceReductionRules_Actions Enabled
+}
+
+Write-Host "Enabling Windows Defender Group Policy Settings"
+Start-Process -NoNewWindow -FilePath ".\Files\LGPO\LGPO.exe" -ArgumentList "/g .\Files\GPO\" -Wait
 
 Write-Host "Updating Signatures..."
-#Update Signatures
-# cd $env:programfiles"\Windows Defender"
-# .\MpCmdRun.exe -removedefinitions -dynamicsignatures
-# .\MpCmdRun.exe -SignatureUpdate
 Update-MpSignature -UpdateSource MicrosoftUpdateServer
 Update-MpSignature -UpdateSource MMPC
 
-Write-Host "Printting Current Windows Defender Configuration"
-# Print Historic Detections
-Get-MpComputerStatus ; Get-MpPreference ; Get-MpThreat ; Get-MpThreatDetection
+Write-Host "Printing Current Windows Defender Configuration"
+Get-MpComputerStatus
+Get-MpPreference
+Get-MpThreat
+Get-MpThreatDetection
 
 Write-Host "Starting Full Scan and removing any known threats..."
-#Start Virus Scan
 Start-MpScan -ScanType FullScan
 
-#Remove Active Threats From System
+Write-Host "Removing Active Threats From System"
 Remove-MpThreat
